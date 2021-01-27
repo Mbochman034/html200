923bd3bde91c841b6e984fb1b969563095af0d17
Mbochman034 <michellebochman@comcast.net>
2021-01-27 10:50:12 -0500

    Additional push

---
+++ lesson04/squared.js
@@ -0,0 +1,9 @@
+function squareRoot() {
+let number = prompt('Enter any number! ') //alert to screen
+
+  function squareRoot(inputNumber) { 
+    return Math.pow(inputNumber,2); 
+  }
+
+alert(squareRoot(number)); 
+}
