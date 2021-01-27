7b7c75980285eaf4955714f678143555d1322b11
Mbochman034 <michellebochman@comcast.net>
2021-01-27 16:34:48 -0500

    Push update

--- lesson04/squared.js
+++ lesson04/squared.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function squareRoot() {
 let number = prompt('Enter any number! ') //alert to screen
 
@@ -7,14 +6,3 @@ let number = prompt('Enter any number! ') //alert to screen
   }
 
 alert(squareRoot(number)); 
-=======
-function squareRoot() {
-let number = prompt('Enter any number! ') //alert to screen
-
-  function squareRoot(inputNumber) { 
-    return Math.pow(inputNumber,2); 
-  }
-
-alert(squareRoot(number)); 
->>>>>>> 60e62fbd0521da3997f1f847de2c1f050fc7386a
-}
